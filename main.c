8b79eea5f568674199a9abc86bfa7c97612086e3
Tomás Maraschio <tomas.maraschio@gmail.com>
2023-08-14 16:04:22 -0300

    handle wrong file formats

--- main.c
+++ main.c
@@ -30,13 +30,19 @@ game_t read_game_from_file(char* filename) {
 
 
     unsigned int x_dim, y_dim;
-    fscanf(fp, "%ux%u", &x_dim, &y_dim);
+    if (fscanf(fp, "%ux%u", &x_dim, &y_dim) != 2) {
+        printf("ERROR: wrong file format\n");
+        return NULL;
+    }
 
     game_t game = create_game(x_dim, y_dim);
 
     while (!feof(fp)) {
         unsigned int x, y;
-        fscanf(fp, "%u,%u", &x, &y);
+        if (fscanf(fp, "%u,%u", &x, &y) != 2) {
+          printf("ERROR: wrong file format\n");
+          return NULL;
+        }
         set_game(game, x, y); 
     }
 
